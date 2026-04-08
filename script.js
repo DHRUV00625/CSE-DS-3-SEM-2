f14631f2bac9096f40868ba7ba3c07b0eaf2f77e
DHRUV SHARMA <dhruv.25b15410073@abes.ac.in>
2026-04-08 14:41:36 +0530

    DHRUV SHARMA MT-1 2503215400058 WEB DESIGNING

---
+++ script.js
@@ -0,0 +1,13 @@
+function checkNumber() {
+    let number = document.getElementById("num").value;
+
+    if (number === "") {
+        document.getElementById("result").innerHTML = "Please enter a number";
+    } 
+    else if (number % 2 === 0) {
+        document.getElementById("result").innerHTML = number + " is Even";
+    } 
+    else {
+        document.getElementById("result").innerHTML = number + " is Odd";
+    }
+}
